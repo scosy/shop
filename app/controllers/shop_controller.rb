f37e081162ce1aa5096b61a9a554cfac16a91809
scosy <amine.bentaleb064@gmail.com>
2022-11-04 11:50:15 +0100

    :sparkles: Added Shop view and controller for show action

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -2,4 +2,14 @@ class ShopController < ApplicationController
   def index
     @shops = Shop.all
   end
+
+  def show
+    @shop = Shop.find(params[:id])
+    @shop_hours = @shop.shop_hours
+    @shop_hour_days = @shop_hours.pluck(:day_of_week)
+    @days = Date::DAYNAMES.dup.map { |day| Date.strptime(day, '%A').wday }
+    until @days.first == Time.now.strftime('%w').to_i
+      @days.rotate!
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "shop#index"
+  
+  resources :shop, only: :show
 end
