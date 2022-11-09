e767b3cf4e0f47e227255855258a7d34bb395839
scosy <amine.bentaleb064@gmail.com>
2022-11-09 16:19:56 +0100

    ✅ Passing test for ShopHour; closes_at must be greater than opens_at

--- app/models/shop_hour.rb
+++ app/models/shop_hour.rb
@@ -2,7 +2,7 @@ class ShopHour < ApplicationRecord
   belongs_to :shop
 
   validates_presence_of :day_of_week, :opens_at, :closes_at
-
+  validates :closes_at, comparison: { greater_than: :opens_at }
   enum :day_of_week, { monday: 1, tuesday: 2, wednesday: 3, thursday: 4, friday: 5, saturday: 6, sunday: 7 }
 
   # Check if new hours overlap with exisiting hours
